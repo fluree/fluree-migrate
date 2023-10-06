a8d72194e92a47937a2f4f03df5bd472f8f75c5b
Andrew Johnson <ajohnson.uva@gmail.com>
2023-10-06 09:15:37 -0400

    implement temp storage to reduce memory footprint

--- src/cli.rs
+++ src/cli.rs
@@ -1,17 +1,6 @@
 pub mod opt {
-    use chrono::{DateTime, NaiveDateTime, Utc};
-    use crossterm::style::{Print, ResetColor, SetForegroundColor};
-    use crossterm::{execute, style::Color};
-    use dialoguer::console::{Style, Term};
     use dialoguer::{theme::ColorfulTheme, Input};
-    use indicatif::{HumanDuration, ProgressBar, ProgressStyle};
-    use reqwest::header::HeaderMap;
-    use serde_json::{json, Value};
-    use std::collections::HashMap;
-    use std::fs::File;
-    use std::io::{self, prelude::*, stdout};
     use std::path::PathBuf;
-    use std::time::Instant;
     use structopt::StructOpt;
 
     #[derive(Debug, StructOpt)]
@@ -51,11 +40,40 @@ pub mod opt {
         #[structopt(long = "create-ledger")]
         pub is_create_ledger: bool,
     }
+
+    impl Opt {
+        pub fn check_url(&self) -> String {
+            match &self.url {
+                Some(url) => url.to_owned(),
+                None => Input::with_theme(&ColorfulTheme::default())
+                    .with_prompt("Fluree DB URL:")
+                    .default("http://localhost:8090/fdb/ledger/name".to_string())
+                    .show_default(true)
+                    .validate_with({
+                        move |input: &String| -> Result<(), &str> {
+                            if let Ok(_) = reqwest::Url::parse(input) {
+                                Ok(())
+                            } else {
+                                Err("Please provide a valid URL")
+                            }
+                        }
+                    })
+                    .interact_text()
+                    .unwrap(),
+            }
+        }
+
+        pub fn validate_namespace_and_context(&self) {
+            if self.namespace.is_some() && self.context.is_none() {
+                panic!("You must specify a context when using the --namespace (-n) flag\n\nExample: -c schema=http://schema.org/ -n schema");
+            }
+        }
+    }
 }
 
 pub mod temp_files {
     use std::fs::{self, File, OpenOptions};
-    use std::io::{self, Read, Write};
+    use std::io::{self, Write};
     use std::path::{Path, PathBuf};
 
     use serde_json::Value;
@@ -69,6 +87,10 @@ pub mod temp_files {
 
     impl TempFile {
         pub fn new(directory: &Path) -> io::Result<Self> {
+            // I want to check if a dir exists and if so, I want to delete all of its contents. If it does not delete, I want to create it
+            if directory.exists() {
+                fs::remove_dir_all(directory)?;
+            }
             fs::create_dir_all(directory)?;
             Ok(TempFile {
                 directory: directory.to_path_buf(),
@@ -130,6 +152,364 @@ pub mod temp_files {
             // }
 
             // Ok(data)
+        }
+    }
+}
+
+pub mod parser {
+    use std::collections::HashMap;
+
+    use serde_json::{Map, Value};
+
+    use crate::{
+        fluree::FlureeInstance,
+        functions::{create_context, standardize_class_name},
+    };
+
+    use self::jsonld::{Class, Property, ShaclShape};
+
+    use super::opt::Opt;
+
+    pub struct Parser {
+        pub classes: HashMap<String, Class>,
+        pub properties: HashMap<String, Property>,
+        pub shacl_shapes: HashMap<String, ShaclShape>,
+        pub prefix: String,
+        pub context: HashMap<String, String>,
+        pub network_name: String,
+        pub db_name: String,
+    }
+
+    impl Parser {
+        pub fn new(prefix: String, opt: &Opt, source_instance: &FlureeInstance) -> Self {
+            let context = create_context(opt, source_instance);
+            Parser {
+                classes: HashMap::new(),
+                properties: HashMap::new(),
+                shacl_shapes: HashMap::new(),
+                prefix,
+                context,
+                network_name: source_instance.network_name.to_owned(),
+                db_name: source_instance.db_name.to_owned(),
+            }
+        }
+
+        pub fn get_vocab_json(&self, opt: &Opt) -> Map<String, Value> {
+            let classes = self
+                .classes
+                .values()
+                .map(|class| serde_json::to_value(class).unwrap());
+
+            let properties: Vec<Value> = self
+                .properties
+                .values()
+                .map(|property| serde_json::to_value(property).unwrap())
+                .collect();
+
+            let class_shacl_shapes: Vec<Value> = self
+                .shacl_shapes
+                .values()
+                .map(|shape| serde_json::to_value(shape).unwrap())
+                .collect();
+
+            let results = match opt.shacl {
+                true => classes
+                    .chain(properties)
+                    .chain(class_shacl_shapes)
+                    .map(|value| value.to_owned())
+                    .collect(),
+                false => classes
+                    .chain(properties)
+                    .map(|value| value.to_owned())
+                    .collect(),
+            };
+
+            let mut vocab_results_map = serde_json::Map::new();
+
+            vocab_results_map.insert(
+                "f:ledger".to_string(),
+                serde_json::json!(format!("{}/{}", self.network_name, self.db_name)),
+            );
+
+            if opt.is_create_ledger {
+                vocab_results_map.insert(
+                    "@context".to_string(),
+                    serde_json::json!({ "f": "https://ns.flur.ee/ledger#"}),
+                );
+                vocab_results_map.insert(
+                    "f:defaultContext".to_string(),
+                    Value::Object(
+                        self.context
+                            .iter()
+                            .map(|(k, v)| (k.to_string(), Value::String(v.to_string())))
+                            .collect(),
+                    ),
+                );
+            } else {
+                vocab_results_map.insert(
+                    "@context".to_string(),
+                    Value::Object(
+                        self.context
+                            .iter()
+                            .map(|(k, v)| (k.to_string(), Value::String(v.to_string())))
+                            .collect(),
+                    ),
+                );
+            }
+
+            vocab_results_map.insert("@graph".to_string(), Value::Array(results));
+
+            vocab_results_map
+        }
+
+        pub fn get_or_create_class(&self, orig_class_name: &str) -> Class {
+            let class_name = &standardize_class_name(orig_class_name, &self.prefix);
+            let class_object = self.classes.get(orig_class_name);
+            let class_object = match class_object {
+                Some(class_object) => class_object.to_owned(),
+                None => Class::new(class_name),
+            };
+            class_object
+        }
+
+        pub fn get_or_create_property(&self, property_name: &str) -> Property {
+            let property_object = self.properties.get(property_name);
+            let property_object = match property_object {
+                Some(property_object) => property_object.to_owned(),
+                None => Property::new(property_name, &self.prefix),
+            };
+            property_object
+        }
+
+        pub fn get_or_create_shacl_shape(&self, class_name: &str) -> ShaclShape {
+            let shacl_shape = self.shacl_shapes.get(class_name);
+            let shacl_shape = match shacl_shape {
+                Some(shacl_shape) => shacl_shape.to_owned(),
+                None => ShaclShape::new(class_name),
+            };
+            shacl_shape
+        }
+    }
+
+    pub mod jsonld {
+        use std::collections::HashMap;
+
+        use serde::{Deserialize, Serialize};
+        use serde_json::Value;
+
+        use crate::functions::{
+            remove_namespace, standardize_class_name, standardize_property_name,
+        };
+
+        #[derive(Debug, Clone, Deserialize, Serialize)]
+        pub struct Class {
+            #[serde(rename = "@id")]
+            pub id: String,
+            #[serde(rename = "@type")]
+            pub type_: String,
+            #[serde(rename = "rdfs:label")]
+            pub label: String,
+            #[serde(rename = "rdfs:comment", skip_serializing_if = "Option::is_none")]
+            pub comment: Option<String>,
+            #[serde(rename = "rdfs:subClassOf", skip_serializing_if = "Option::is_none")]
+            pub sub_class_of: Option<Vec<HashMap<String, String>>>,
+            #[serde(rename = "rdfs:range")]
+            pub range: Vec<HashMap<String, String>>,
+        }
+
+        impl Class {
+            pub fn new(class_name: &str) -> Self {
+                Class {
+                    id: class_name.to_string(),
+                    type_: "rdfs:Class".to_string(),
+                    label: remove_namespace(class_name).to_string(),
+                    comment: None,
+                    sub_class_of: None,
+                    range: Vec::new(),
+                }
+            }
+
+            pub fn set_property_range(&mut self, property_name: &str) {
+                self.range.push(HashMap::from([(
+                    "@id".to_string(),
+                    property_name.to_string(),
+                )]));
+            }
+        }
+
+        #[derive(Debug, Clone, Serialize, Deserialize)]
+        pub struct Property {
+            #[serde(rename = "@id")]
+            pub id: String,
+            #[serde(rename = "@type")]
+            pub type_: String,
+            #[serde(rename = "rdfs:label")]
+            pub label: String,
+            #[serde(rename = "rdfs:comment")]
+            pub comment: String,
+            #[serde(rename = "rdfs:domain")]
+            pub domain: Vec<HashMap<String, String>>,
+        }
+
+        impl Property {
+            pub fn new(property_name: &str, prefix: &str) -> Self {
+                let standard_property_name = standardize_property_name(property_name, prefix);
+                Property {
+                    id: standard_property_name.clone(),
+                    type_: "rdf:Property".to_string(),
+                    label: remove_namespace(&standard_property_name),
+                    comment: String::new(),
+                    domain: Vec::new(),
+                }
+            }
+
+            pub fn set_class_domain(&mut self, class_name: &str) {
+                self.domain
+                    .push(HashMap::from([("@id".to_string(), class_name.to_string())]));
+            }
+        }
+
+        #[derive(Debug, Clone, Serialize, Deserialize)]
+        pub struct ShaclShape {
+            #[serde(rename = "@id", skip_serializing_if = "String::is_empty")]
+            pub id: String,
+            #[serde(rename = "@type")]
+            pub type_: String,
+            #[serde(rename = "sh:targetClass")]
+            pub target_class: HashMap<String, String>,
+            #[serde(rename = "sh:targetProperty", skip_serializing_if = "String::is_empty")]
+            pub target_property: String,
+            #[serde(rename = "sh:property")]
+            pub property: Vec<ShaclProperty>,
+            #[serde(rename = "sh:datatype", skip_serializing_if = "String::is_empty")]
+            pub datatype: String,
+            #[serde(rename = "sh:nodeKind", skip_serializing_if = "String::is_empty")]
+            pub node_kind: String,
+        }
+
+        impl ShaclShape {
+            pub fn new(class_name: &str) -> Self {
+                ShaclShape {
+                    id: String::new(),
+                    type_: "sh:NodeShape".to_string(),
+                    // new HashMap with initial values { "id": class_name }
+                    target_class: {
+                        let mut map = HashMap::new();
+                        map.insert("@id".to_string(), class_name.to_string());
+                        map
+                    },
+                    target_property: String::new(),
+                    property: Vec::new(),
+                    datatype: String::new(),
+                    node_kind: String::new(),
+                }
+            }
+
+            pub fn set_property(
+                &mut self,
+                property_object: &mut Property,
+                item: &Value,
+                prefix: &str,
+            ) {
+                let mut shacl_property = ShaclProperty::new(&property_object.id);
+
+                if item["multi"].is_null() || !item["multi"].as_bool().unwrap() {
+                    shacl_property.max_count = Some(1);
+                }
+
+                let keys = item.as_object().unwrap().keys();
+
+                for key in keys {
+                    match key.as_str() {
+                        // "_id" => {}
+                        "doc" => {
+                            property_object.comment = item["doc"].as_str().unwrap().to_string();
+                        }
+                        "type" => {
+                            let type_value = item["type"].as_str().unwrap();
+                            match type_value {
+                                "float" | "int" | "instant" | "boolean" | "long" | "string"
+                                | "ref" => {
+                                    let data_type = match type_value {
+                                        "int" => "xsd:integer".to_string(),
+                                        "instant" => "xsd:dateTime".to_string(),
+                                        "ref" => "xsd:anyURI".to_string(),
+                                        _ => format!("xsd:{}", type_value),
+                                    };
+                                    shacl_property.datatype =
+                                        Some(HashMap::from([("@id".to_string(), data_type)]));
+                                }
+                                "tag" => {
+                                    // TODO: Figure out how to handle tag types
+                                }
+                                _ => {}
+                            }
+                        }
+                        "restrictCollection" => {
+                            shacl_property.class = Some(HashMap::from([(
+                                "@id".to_string(),
+                                standardize_class_name(
+                                    item["restrictCollection"].as_str().unwrap(),
+                                    &prefix,
+                                ),
+                            )]));
+                        }
+                        "restrictTag" => {
+                            // this is a boolean
+                        }
+                        "unique" => {}
+                        "index" => {}
+                        "fullText" => {}
+                        "upsert" => {}
+                        _ => {}
+                    }
+                }
+                self.property.push(shacl_property);
+            }
+        }
+
+        #[derive(Debug, Clone, Serialize, Deserialize)]
+        pub struct ShaclProperty {
+            #[serde(rename = "@id", skip_serializing_if = "String::is_empty")]
+            pub id: String,
+            #[serde(rename = "@type", skip_serializing_if = "String::is_empty")]
+            pub type_: String,
+            #[serde(rename = "rdfs:label", skip_serializing_if = "String::is_empty")]
+            pub label: String,
+            #[serde(rename = "rdfs:comment", skip_serializing_if = "String::is_empty")]
+            pub comment: String,
+            #[serde(rename = "sh:path", skip_serializing_if = "HashMap::is_empty")]
+            pub path: HashMap<String, String>,
+            #[serde(rename = "sh:class", skip_serializing_if = "Option::is_none")]
+            pub class: Option<HashMap<String, String>>,
+            #[serde(rename = "sh:minCount", skip_serializing_if = "Option::is_none")]
+            pub min_count: Option<u32>,
+            #[serde(rename = "sh:maxCount", skip_serializing_if = "Option::is_none")]
+            pub max_count: Option<u32>,
+            #[serde(rename = "sh:datatype", skip_serializing_if = "Option::is_none")]
+            pub datatype: Option<HashMap<String, String>>,
+            #[serde(rename = "sh:nodeKind", skip_serializing_if = "String::is_empty")]
+            pub node_kind: String,
+            #[serde(rename = "sh:pattern", skip_serializing_if = "String::is_empty")]
+            pub pattern: String,
+        }
+
+        impl ShaclProperty {
+            pub fn new(property_name: &str) -> Self {
+                ShaclProperty {
+                    id: String::new(),
+                    type_: String::new(),
+                    label: String::new(),
+                    comment: String::new(),
+                    path: HashMap::from([("@id".to_string(), property_name.to_string())]),
+                    class: None,
+                    min_count: None,
+                    max_count: None,
+                    datatype: None,
+                    node_kind: String::new(),
+                    pattern: String::new(),
+                }
+            }
         }
     }
 }
---
+++ src/functions.rs
@@ -0,0 +1,226 @@
+use chrono::{DateTime, NaiveDateTime, Utc};
+use crossterm::execute;
+use crossterm::style::{Print, ResetColor};
+use serde_json::Value;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::{self, prelude::*, stdout};
+use std::path::PathBuf;
+
+use crate::cli::opt::Opt;
+// use crate::cli::opt::Opt;
+use crate::fluree::FlureeInstance;
+
+// I have epoch instant values like 1693403567000 but want to convert them to ISO strings like "2023-08-30T13:52:47.000Z"
+pub fn instant_to_iso_string(epoch: i64) -> String {
+    let naive =
+        NaiveDateTime::from_timestamp_millis(epoch).expect("DateTime value is out of range");
+    let date_time: DateTime<Utc> = DateTime::from_naive_utc_and_offset(naive, Utc);
+    date_time.to_rfc3339_opts(chrono::SecondsFormat::Millis, true)
+}
+
+pub fn represent_fluree_value(value: &Value) -> Value {
+    match value {
+        Value::Object(value) => serde_json::json!({ "@id": value["_id"].to_string() }),
+        Value::Array(value) => {
+            let mut array: Vec<Value> = Vec::new();
+            for item in value {
+                array.push(represent_fluree_value(item));
+            }
+            Value::Array(array)
+        }
+        Value::String(value) => Value::String(value.to_string()),
+        Value::Number(value) => Value::Number(value.to_owned()),
+        Value::Bool(value) => Value::Bool(value.to_owned()),
+        Value::Null => Value::Null,
+    }
+}
+
+// a function that expects strings. If the string has a pattern of substr:substr separated by ":", then it will return the second substr
+pub fn remove_namespace(string: &str) -> String {
+    let mut split = string.split(":");
+    let first = split.next();
+    let second = split.next();
+    match (first, second) {
+        (Some(_), Some(second)) => second,
+        _ => string,
+    }
+    .to_owned()
+}
+
+// remove leading "_" from a string, so "_fn" should return "fn", "_auth" should return "auth", etc.
+// pub fn remove_underscore(string: &str) -> &str {
+//     if string.starts_with("_") {
+//         &string[1..]
+//     } else {
+//         string
+//     }
+// }
+
+// standardize a string so that it goes from snake_case to CamelCase. The one exception is that if the leading char is uncapitalized, it should remain uncapitalized
+pub fn case_normalize(string: &str) -> String {
+    let mut split = string.split("_");
+    let mut result = String::new();
+    let first = split.next();
+    let first = match first {
+        Some(first) => first,
+        None => "",
+    };
+    result.push_str(first);
+    for part in split {
+        result.push_str(&capitalize(part));
+    }
+    result
+}
+
+pub fn capitalize(string: &str) -> String {
+    let mut chars = string.chars();
+    match chars.next() {
+        None => String::new(),
+        Some(first_char) => first_char.to_uppercase().collect::<String>() + chars.as_str(),
+    }
+}
+
+pub fn add_prefix(string: &str, prefix: &str) -> String {
+    format!("{}{}", prefix, string)
+}
+
+pub fn standardize_class_name(string: &str, prefix: &str) -> String {
+    let string = remove_namespace(string);
+    let string = capitalize(&string);
+    let string = case_normalize(&string);
+    add_prefix(&string, prefix)
+}
+
+pub fn standardize_property_name(string: &str, prefix: &str) -> String {
+    let string = case_normalize(string);
+    add_prefix(&string, prefix)
+}
+
+pub fn parse_current_predicates(json: Value) -> Value {
+    let pre_reduce_preds = json["current_predicates"].as_array().unwrap();
+    let initial_predicates = json["initial_predicates"]
+        .as_array()
+        .unwrap()
+        .iter()
+        .map(|value| value.as_i64().unwrap())
+        .collect::<Vec<i64>>();
+    let current_predicates = pre_reduce_preds
+        .iter()
+        .filter(|value| {
+            let id = value["_id"].as_i64().unwrap();
+            !initial_predicates.contains(&id)
+        })
+        .collect::<Vec<&Value>>();
+    serde_json::json!(current_predicates)
+}
+
+pub fn parse_contexts(contexts: &Vec<String>) -> HashMap<String, String> {
+    let mut context = HashMap::<String, String>::new();
+    for context_string in contexts {
+        let mut context_parts = context_string.split("=");
+        let context_key = context_parts.next().expect("When using --context (-c), you must specify a key and value separated by an equals sign (e.g. --context schema=http://schema.org/)");
+        let context_value = context_parts.next().expect("When using --context (-c), you must specify a key and value separated by an equals sign (e.g. --context schema=http://schema.org/)");
+        context.insert(context_key.to_string(), context_value.to_string());
+    }
+    context
+}
+
+pub fn create_context(opt: &Opt, source_instance: &FlureeInstance) -> HashMap<String, String> {
+    let mut context: HashMap<String, String> = HashMap::new();
+    if let Some(contexts) = &opt.context {
+        context = parse_contexts(&contexts);
+    }
+
+    match &opt.base {
+        Some(base) => {
+            context.insert("@base".to_string(), base.clone());
+        }
+        None => {
+            if opt.namespace.is_none() {
+                context.insert(
+                    "@base".to_string(),
+                    format!("{}/terms/", source_instance.url),
+                );
+            }
+        }
+    }
+
+    if opt.shacl {
+        context.insert("sh".to_string(), "http://www.w3.org/ns/shacl#".to_string());
+        context.insert(
+            "xsd".to_string(),
+            "http://www.w3.org/2001/XMLSchema#".to_string(),
+        );
+    }
+
+    context.insert(
+        "rdfs".to_string(),
+        "http://www.w3.org/2000/01/rdf-schema#".to_string(),
+    );
+    context.insert(
+        "rdf".to_string(),
+        "http://www.w3.org/1999/02/22-rdf-syntax-ns#".to_string(),
+    );
+    context.insert("f".to_string(), "https://ns.flur.ee/ledger#".to_string());
+    context
+}
+
+pub fn parse_for_class_and_property_name(item: &Value) -> (String, String) {
+    let item_id = item["_id"]
+        .as_i64()
+        .expect("An item in the JSON array does not have an _id");
+    let item_name = item["name"].as_str().expect(
+        format!(
+            "An item in the JSON array does not have a name: {:?}",
+            item_id
+        )
+        .as_str(),
+    );
+    let mut name_split = item_name.split("/");
+    let name_parts: [&str; 2] = [
+        name_split.next().expect(
+            format!(
+                "{} does not have a collection and property name (e.g. collection/property)",
+                item_name
+            )
+            .as_str(),
+        ),
+        name_split.next().expect(
+            format!(
+                "{} does not have a collection and property name (e.g. collection/property)",
+                item_name
+            )
+            .as_str(),
+        ),
+    ];
+
+    let orig_class_name = name_parts[0].to_string();
+    let orig_property_name = name_parts[1].to_string();
+    (orig_class_name, orig_property_name)
+}
+
+pub fn write_or_print<P>(output: &Option<PathBuf>, file_name: P, data: String)
+where
+    P: AsRef<std::path::Path>,
+{
+    match output {
+        Some(output) => {
+            std::fs::create_dir_all(output).unwrap_or_else(|why| {
+                if why.kind() != std::io::ErrorKind::AlreadyExists {
+                    panic!("Unable to create output directory: {}", why);
+                }
+            });
+
+            let mut file = File::create(output.join(file_name)).expect("Unable to create file");
+            let mut data_writer = io::BufWriter::new(&mut file);
+            data_writer
+                .write_all(data.as_bytes())
+                .expect("Unable to write data");
+        }
+        None => {
+            let mut stdout = stdout();
+            execute!(stdout, Print(data), ResetColor).unwrap();
+        }
+    }
+}
